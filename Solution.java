f5be5634d8af53d7b4a5d39a286ae72dd2f8d8fe
sdim-ua <ingersol@yandex.ua>
2019-03-12 23:45:50 +0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -62,10 +62,82 @@ public class Solution {
 
     public static void printPets(Set<Object> pets) {
         //напишите тут ваш код
+    package com.javarush.task.task08.task0820;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+
+/* 
+Множество всех животных
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+        Set<Dog> dogs = createDogs();
+
+        Set<Object> pets = join(cats, dogs);
+        printPets(pets);
+
+        removeCats(pets, cats);
+        printPets(pets);
+    }
+
+    public static Set<Cat> createCats() {
+        HashSet<Cat> result = new HashSet<Cat>();
+
+        //напишите тут ваш код
+        
+       
+        result.add(new Cat());
+        result.add(new Cat()); 
+        result.add(new Cat()); 
+        result.add(new Cat()); 
+
+        return result;
+    }
+
+    public static Set<Dog> createDogs() {
+        //напишите тут ваш код
+         HashSet <Dog>  result = new HashSet<>();
+         result.add(new Dog());
+         result.add(new Dog());
+         result.add(new Dog());
+        
+        return result;
+    }
+
+    public static Set<Object> join(Set<Cat> cats, Set<Dog> dogs) {
+        //напишите тут ваш код
+        HashSet<Object> result = new HashSet<Object>();
+        
+        result.addAll(cats);
+        result.addAll(dogs);
+           
+        return result;
+    }
+
+    public static void removeCats(Set<Object> pets, Set<Cat> cats) {
+        //напишите тут ваш код
+        pets.removeAll(cats);
+        
+    }
+
+    public static void printPets(Set<Object> pets) {
+        //напишите тут ваш код
         Iterator iterator = pets.iterator();
         while (iterator.hasNext())
             System.out.println(iterator.next());
     }
 
     //напишите тут ваш код
+     public static class Cat{
+        
+     
+    }
+     public static class Dog{
+        
+     
+    }
 }
